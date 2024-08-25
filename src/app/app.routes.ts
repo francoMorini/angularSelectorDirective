9f4b29605519bb954f358c8c1b0464f573d6292f
Franco <fnm.franco@gmail.com>
2024-08-25 03:15:10 +0200

    joke

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
-import { Routes } from '@angular/router';
-import { LandingComponent } from './pages/landing/landing.component';
+import { Routes } from "@angular/router";
+import { LandingComponent } from "./pages/landing/landing.component";
+import { JokeComponent } from "./pages/joke/joke.component";
 
 export const routes: Routes = [
-    {path: 'landing', component: LandingComponent},
-    {path: '', redirectTo: 'landing', pathMatch: 'full'}
+  { path: "landing", component: LandingComponent },
+  { path: "", redirectTo: "landing", pathMatch: "full" },
+  { path: "dev", component: JokeComponent },
 ];
